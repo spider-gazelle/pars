64ad84e803e69c686e781fc202adf37e90ce011a
Kim Burgess <kim@place.technology>
2020-10-06 21:41:33 +1000

    refactor: neaten up Parse module

--- src/pars3k/parse.cr
+++ src/pars3k/parse.cr
@@ -3,30 +3,18 @@ require "./parse_result"
 
 module Pars3k
   module Parse
-    # ```cr
-    # p = Parse.constant "abc"
-    # puts p.parse "abc" #=> abc
-    # puts p.parse "123" #=> abc
-    # puts p.parse "" #=> abc
-    # ```
-    #
-    # `Parse.constant(value : T)` creates a `Parser(T)` that always succeeds with `value`.
-    # This method is used to create final parse results using the `do_parse` macro.
-    def self.constant(value : T) : Parser(T) forall T
-      Parser(T).new do |ctx|
-        ParseResult(T).new value, ctx
+    extend self
+
+    # Creates a `Parser(T)` that always succeeds with `value`.
+    def constant(value : T) : Parser(T) forall T
+      Parser(T).new do |context|
+        ParseResult(T).new value, context
       end
     end
 
-    # ```cr
-    # p = Parse.char 'a'
-    # puts p.parse "abc" #=> a
-    # puts p.parse "bca" #=> expected 'a', got 'b'
-    # puts p.parse "cab" #=> expected 'a', got 'c'
-    # ```
-    #
-    # `Parse.char(c : Char)` creates a `Parser(Char)` that looks at the current parse position and expects `c`.
-    def self.char(char : Char)
+    # Creates a `Parser(Char)` that looks at the current parse position and
+    # expects `c`.
+    def char(char : Char)
       Parser(Char).new do |context|
         if context.position >= context.parsing.size
           ParseResult(Char).error "expected '#{char}', input ended", context
@@ -38,20 +26,14 @@ module Pars3k
       end
     end
 
-    # ```cr
-    # p = Parse.string "cat"
-    # puts p.parse "cat" #=> cat
-    # puts p.parse "dog" #=> expected 'c', got 'd'
-    # puts p.parse "" #=> expected 'c', got end of string
-    # ```
-    #
-    # `Parse.string(s : String)` creates a `Parser(String)` that looks at the current parse position expects
-    # the array of characters in the string `s` (`s.chars`) to be consecutively present.
-    def self.string(string : String) : Parser(String)
+    # Creates a `Parser(String)` that looks at the current parse position
+    # expects the array of characters in the string `s` (`s.chars`) to be
+    # consecutively present.
+    def string(string : String) : Parser(String)
       if string.size == 0
         constant ""
       elsif string.size == 1
-        (char string[0]).transform { |c| c.to_s }
+        (char string[0]).transform &.to_s
       else
         parser = char string[0]
         string[1...string.size].chars.each do |char|
@@ -61,17 +43,9 @@ module Pars3k
       end
     end
 
-    # ```cr
-    # p = Parse.one_char_of "abc"
-    # puts p.parse "apple" #=> a
-    # puts p.parse "banana" #=> b
-    # puts p.parse "carrot" #=> c
-    # puts p.parse "dragonfruit" #=> expected 'c', got 'd'
-    # ```
-    #
-    # `Parse.one_char_of(s : String)` creates a `Parser(Char)` that looks at the current parse position and
+    # Creates a `Parser(Char)` that looks at the current parse position and
     # expects the current character to be present in the string `s`.
-    def self.one_char_of(string : String) : Parser(Char)
+    def one_char_of(string : String) : Parser(Char)
       parser = char string[0]
       (1...string.size).each do |index|
         parser |= char string[index]
@@ -79,17 +53,9 @@ module Pars3k
       parser
     end
 
-    # ```cr
-    # p = Parse.no_char_of "abc"
-    # puts p.parse "apple" #=> expected no character of 'abc', got 'a'
-    # puts p.parse "banana" #=> expected no character of 'abc', got 'b'
-    # puts p.parse "carrot" #=> expected no character of 'abc', got 'c'
-    # puts p.parse "dragonfruit" #=> d
-    # ```
-    #
-    # `Parse.no_char_of(s : String)` functions identically to `Parse.one_char_of`, but reverses the expected
+    # Functions identically to `Parse.one_char_of`, but reverses the expected
     # input. If the current character is present in `s`, then the parse fails.
-    def self.no_char_of(string : String) : Parser(Char)
+    def no_char_of(string : String) : Parser(Char)
       Parser(Char).new do |context|
         if context.position >= context.parsing.size
           ParseResult(Char).error "expected none of '#{string}', input ended", context
@@ -101,18 +67,9 @@ module Pars3k
       end
     end
 
-    # ```cr
-    # char_a = Parse.char 'a'
-    # many_space = Parse.many_of char_a
-    # puts p.parse "abc" #=> ['a']
-    # puts p.parse "aabbcc" #=> ['a', 'a']
-    # puts p.parse "aaaaaah" #=> ['a', 'a', 'a', 'a', 'a', 'a']
-    # puts p.parse "pars3k" #=> []
-    # ```
-    #
-    # `Parse.many_of(p : Parser(T))` creates a `Parser(Array(T))` that continuously parses the parser `p`
-    # until it fails. It succeeds with an array of the successive values.
-    def self.many_of(parser : Parser(T)) : Parser(Array(T)) forall T
+    # Creates a `Parser(Array(T))` that continuously parses the parser `p` until
+    # it fails. It succeeds with an array of the successive values.
+    def many_of(parser : Parser(T)) : Parser(Array(T)) forall T
       Parser(Array(T)).new do |ctx|
         result = parser.block.call ctx
         results = [] of T
@@ -128,19 +85,10 @@ module Pars3k
       end
     end
 
-    # ```cr
-    # char_a = Parse.one_char_of "act"
-    # valid = Parse.one_or_more_of char_a
-    # puts valid.parse "cat" #=> ['c', 'a', 't']
-    # puts valid.parse "act" #=> ['a', 'c', 't']
-    # puts valid.parse "t" #=> ['t']
-    # puts valid.parse "nope" #=> expected 't', got 'n'
-    # ```
-    #
-    # `Parse.one_or_more_of(p : Parser(T))` creates a `Parser(Array(T))` that works like `Parse.many_of`,
-    # but expects at least one parse to succeed. Returns with the error of the first failure if it does
-    # not succeed.
-    def self.one_or_more_of(parser : Parser(T)) : Parser(Array(T)) forall T
+    # Creates a `Parser(Array(T))` that works like `Parse.many_of`, but expects
+    # at least one parse to succeed. Returns with the error of the first failure
+    # if it does not succeed.
+    def one_or_more_of(parser : Parser(T)) : Parser(Array(T)) forall T
       Parser(Array(T)).new do |context|
         result = parser.block.call context
         if result.errored
@@ -157,21 +105,11 @@ module Pars3k
       end
     end
 
-    # ```cr
-    # char_a = Parse.char 'a'
-    # set = Parse.some_of char_a, 2..4
-    # puts set.parse "" #=> expected 'a', got end of string
-    # puts set.parse "a" #=> expected 'a', got end of string
-    # puts set.parse "aa" #=> ['a', 'a']
-    # puts set.parse "aaa" #=> ['a', 'a', 'a']
-    # puts set.parse "aaaa" #=> ['a', 'a', 'a', 'a']
-    # puts set.parse "aaaaa" #=> ['a', 'a', 'a', 'a']
-    # ```
-    #
-    # `Parse.some_of(p : Parser(T), r : Range)` creates a `Parser(Array(T))` that works like `Parse.many_of`,
-    # but fails if the number of successful parses is below the lower bound of the range `r`, and stops parsing
-    # if the number of successful parses goes over the limit. In this case, `char_a` is parsed between 2 and 4 times.
-    def self.some_of(parser : Parser(T), range : Range) : Parser(Array(T)) forall T
+    # Creates a `Parser(Array(T))` that works like `Parse.many_of`, but fails if
+    # the number of successful parses is below the lower bound of the range `r`,
+    # and stops parsing if the number of successful parses goes over the limit.
+    # In this case, `char_a` is parsed between 2 and 4 times.
+    def some_of(parser : Parser(T), range : Range) : Parser(Array(T)) forall T
       max = range.end - (range.excludes_end? ? 1 : 0)
       Parser(Array(T)).new do |ctx|
         count = 0
@@ -200,34 +138,19 @@ module Pars3k
     end
 
     # Runs `Parse.some_of(p, count..count)`.
-    def self.some_of(parser : Parser(T), count : Int32) : Parser(Array(T)) forall T
+    def some_of(parser : Parser(T), count : Int32) : Parser(Array(T)) forall T
       some_of(parser, count..count)
     end
 
-    # ```cr
-    # char_a = Parse.char 'a'
-    # set = Parse.one_of char_a
-    # puts set.parse "" #=> []
-    # puts set.parse "a" #=> ['a']
-    # puts set.parse "aa" #=> ['a']
-    # ```
-    #
     # `Parse.one_of(p : Parser(T))` is a shortcut to `Parse.some_of(p, 0..1)`.
-    def self.one_of(parser : Parser(T)) : Parser(Array(T)) forall T
+    def one_of(parser : Parser(T)) : Parser(Array(T)) forall T
       some_of parser, ..1
     end
 
-    # ```cr
-    # set = Parse.one_of? Parse.char 'a'
-    # puts set.parse "" #=> nil
-    # puts set.parse "a" #=> a
-    # puts set.parse "aa" #=> a
-    # ```
-    #
-    # `Parse.one_of?(p : Parser(T))` creates a `Parser(T | Nil)` that will return nil if no parse is found.
-    # Otherwise, it returns the value of `T`. To use the result effectively, check the return type with
-    # `result.nil?`.
-    def self.one_of?(parser : Parser(T)) : Parser(T?) forall T
+    # Creates a `Parser(T | Nil)` that will return nil if no parse is found.
+    # Otherwise, it returns the value of `T`. To use the result effectively,
+    # check the return type with `result.nil?`.
+    def one_of?(parser : Parser(T)) : Parser(T?) forall T
       Parser(T?).new do |context|
         result = parser.block.call context
         if result.errored
@@ -238,21 +161,10 @@ module Pars3k
       end
     end
 
-    # ```cr
-    # parser = (Parse.one_of? Parse.char 'a').sequence do |a|
-    #   (Parse.if_not_nil? Parse.char('b'), char).sequence do |b|
-    #     Parse.constant({a, b})
-    #   end
-    # end
-    # puts parser.parse "ab" #=> {'a', 'b'}
-    # puts parser.parse "ac" #=> expected 'b', got 'c'
-    # puts parser.parse "b" #=> {nil, nil}
-    # ```
-    #
-    # `Parse.if_not_nil?(p : Parser(T), value : B?)` creates a `Parser(T | Nil)` that will always return nil
-    # if the initial `value` is nil. This is to be used with `do_parse` or `Parser#sequence`. It can be used to
-    # ignore parsers if one before it yielded a nil value.
-    def self.if_not_nil?(parser : Parser(T), value : B) : Parser(T?) forall T, B
+    # Reates a `Parser(T | Nil)` that will always return nil if the initial
+    # `value` is nil. This is to be used with `do_parse` or `Parser#sequence`.
+    # It can be used to ignore parsers if one before it yielded a nil value.
+    def if_not_nil?(parser : Parser(T), value : B) : Parser(T?) forall T, B
       if value.nil?
         Parser(T?).new { |context| ParseResult(T?).new(nil, context) }
       else
@@ -267,20 +179,9 @@ module Pars3k
       end
     end
 
-    # ```cr
-    # whitespace = Parse.many_of Parse.char ' '
-    # comma = whitespace >> Parse.char(',') >> whitespace
-    # word = Parse.join Parse.one_or_more_of Parse.one_char_of "abcdefghijklmnopqrstuvwxyz01234567890"
-    # list = Parse.delimited_list word, comma
-    # puts list.parse "" #=> []
-    # puts list.parse "hello, world" #=> ["hello", "world"]
-    # puts list.parse "par ,  s3k" #=> ["par", "s3k"]
-    # puts list.parse "!!!, hello" #=> []
-    # ```
-    #
-    # `Parse.delimited_list(p : Parser(T), d : Parser(B))` creates a `Parser(Array(T))` that will continue to parse
-    # with `p` delimited by `d` until an error with either occurs.
-    def self.delimited_list(parser : Parser(A), delimiter : Parser(B)) : Parser(Array(A)) forall A, B
+    # Creates a `Parser(Array(T))` that will continue to parse with *parser*
+    # delimited by *delimter* until an error with either occurs.
+    def delimited_list(parser : Parser(A), delimiter : Parser(B)) : Parser(Array(A)) forall A, B
       Parser(Array(A)).new do |ctx|
         result = parser.block.call ctx
         if result.errored
@@ -303,85 +204,44 @@ module Pars3k
       end
     end
 
-    # ```cr
-    # alphabet = Parse.one_char_of "abcdefghijklmnopqrstuvwxyz"
-    # word = Parse.many_of alphabet
-    # word_joined = Parse.join word
-    # puts word.parse "hello" #=> ['h', 'e', 'l', 'l', 'o']
-    # puts word_joined.parse "hello" #=> hello
-    # ```
-    #
-    # `Parse.join(p : Parser(Char))` transforms the parser `p` by adding all of the characters of a result
-    # into a string.
-    def self.join(parser : Parser(Array(Char))) : Parser(String)
+    # Transforms *parser* by adding all of the characters of a result into a
+    # string.
+    def join(parser : Parser(Array(Char))) : Parser(String)
       parser.transform &.reduce "" { |v, c| v + c }
     end
 
-    # ```cr
-    # Parse.one_char_of "abcdefghijklmnopqrstuvwxyz"
-    # ```
-    #
     # Parses a character of the lowercase alphabet.
-    def self.alphabet_lower
+    def alphabet_lower
       one_char_of "abcdefghijklmnopqrstuvwxyz"
     end
 
-    # ```cr
-    # Parse.one_char_of "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    # ```
-    #
     # Parses a character of the uppercase alphabet.
-    def self.alphabet_upper
+    def alphabet_upper
       one_char_of "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     end
 
-    # ```cr
-    # Parse.alphabet_lower | Parse.alphabet_upper
-    # ```
-    #
     # Parses a character in the alphabet regardless of case.
-    def self.alphabet
+    def alphabet
       alphabet_lower | alphabet_upper
     end
 
-    # ```cr
-    # Parse.join Parse.one_or_more_of Parse.alphabet
-    # ```
-    #
     # Parses a full word of at least one character.
-    def self.word
+    def word
       (one_or_more_of alphabet).transform &.join
     end
 
-    # ```cr
-    # Parse.one_char_of "0123456789"
-    # ```
-    #
     # Parses a digit as a character.
-    def self.digit
+    def digit
       one_char_of "0123456789"
     end
 
-    # ```cr
-    # (Parse.join Parse.one_or_more_of Parse.digit).transform &.to_i
-    # ```
-    #
     # Parses an integer as an actual `Int`.
-    def self.int
+    def int
       (one_or_more_of digit).transform &.join
     end
 
-    # ```cr
-    # do_parse({
-    #   whole <= (Parse.join Parse.one_or_more_of Parse.digit),
-    #   _ <= (Parse.one_of? char '.'),
-    #   decimal <= (Parse.join Parse.many_of Parse.digit),
-    #   Parse.constant "#{whole}#{decimal.size == 0 ? ".0" : "." + decimal}".to_f
-    # })
-    # ```
-    #
     # Parses a float as an actual `Float`.
-    def self.float
+    def float
       do_parse({
         whole <= (join one_or_more_of digit),
         _ <= (one_of? char '.'),
