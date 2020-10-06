7b7907b2634da8e7cd0832bd8e0da50a0b735968
Kim Burgess <kim@place.technology>
2020-10-06 21:15:00 +1000

    style: neaten up parser

--- src/pars3k/parser.cr
+++ src/pars3k/parser.cr
@@ -2,28 +2,29 @@ require "./parse_result"
 require "./parse_context"
 
 module Pars3k
-    # Parser(T) is a parser with return type T.
   class Parser(T)
-    getter block
+    private getter block
 
     def initialize(&block : ParseContext -> ParseResult(T))
       @block = block
     end
 
-    # Parses the input string `input` given the parser's logic provided
-    # by its block at definition.
+    # Parses the input string `input` given the parser's logic provided by its
+    # block at definition.
     def parse(input : String) : (T | ParseError)
       context = ParseContext.new input
       result = @block.call context
       result.value
     end
 
-    # Transforms the result of the parser such that, when the parser is
-    # parsed, the output value becomes a different value.
-    # For example, if you took a Parser(Char) and wanted to transform
-    # it to a Parser(String) by `Char.to_s`, then you could use
-    # `Parser(Char).transform { |char| char.to_s }`. It is similar to
-    # a map method on arrays from other languages.
+    # Transforms the result of the parser such that, when the parser runs, the
+    # output value becomes a different value.
+    #
+    # For example, if you took a `Parser(Char)` and wanted to transform it to a
+    # `Parser(String)` by `Char#to_s`, then you could use
+    # `char_parser.transform &.to_s`.
+    #
+    # It is similar to a map method on arrays from other languages.
     def transform(&new_block : T -> B) : Parser(B) forall B
       Parser(B).new do |context|
         result = @block.call context
@@ -39,9 +40,10 @@ module Pars3k
       end
     end
 
-    # Sequences the current parser with another parser.
-    # Expects a block that receives the result of the current parser
-    # and returns a new parser of any type, presumably dynamically created.
+    # Sequences `self` with another parser.
+    #
+    # Expects a block that receives the result of the current parser and returns
+    # a new parser of any type.
     def sequence(&new_block : T -> Parser(B)) : Parser(B) forall B
       Parser(B).new do |context|
         result = @block.call context
@@ -54,13 +56,13 @@ module Pars3k
       end
     end
 
-    # Sequences the current parser with another parser given they are the same type.
+    # Sequences `self` with another parser of the same type.
     def +(other : Parser(B)) : Parser(B) forall B
       sequence { |_| other }
     end
 
-    # Sequences the current parser with another parser, and disregards the other parser's result,
-    # but ensures the two succeed.
+    # Sequences the current parser with another parser, and disregards the other
+    # parser's result, but ensures the two succeed.
     def <<(other : Parser(B)) : Parser(T) forall B
       Parser(T).new do |context|
         result = @block.call context
@@ -78,8 +80,8 @@ module Pars3k
       end
     end
 
-    # Sequences the current parser with another parser, and disregards the original parser's result,
-    # but ensures the two succeed.
+    # Sequences the current parser with another parser, and disregards the
+    # original parser's result, but ensures the two succeed.
     def >>(other : Parser(B)) : Parser(B) forall B
       Parser(B).new do |context|
         result = @block.call context
@@ -92,9 +94,8 @@ module Pars3k
       end
     end
 
-    # Given `A | B`, creates a new parser that succeeds when
-    # A succeeds or B succeeds. Checks A first, doesn't check B
-    # if A succeeds.
+    # Given `A | B`, creates a new parser that succeeds when A succeeds or B
+    # succeeds. Checks A first, doesn't check B if A succeeds.
     def |(other : Parser(T)) : Parser(T)
       Parser(T).new do |context|
         result = @block.call context
@@ -106,9 +107,9 @@ module Pars3k
       end
     end
 
-    # Given `A / B`, creates a new parser that succeeds when
-    # A succeeds or B succeeds. Checks A first, doesn't check B
-    # if A succeeds. Ignores type differences, gives union type.
+    # Given `A / B`, creates a new parser that succeeds when A succeeds or B
+    # succeeds. Checks A first, doesn't check B if A succeeds. Ignores type
+    # differences, gives union type.
     def /(other : Parser(B)) : Parser(T | B) forall B
       Parser(T | B).new do |context|
         result = @block.call context
